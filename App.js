d0ce5cc61c0e1677640422070d8d75cb7cf39f85
Manon POTEL <manonpotel@hotmail.fr>
2023-01-24 15:46:54 +0100

    Menu burger en construction => Commenter pour cause d'erreur le 24/01/2023

--- App.js
+++ App.js
@@ -1,22 +1,30 @@
 import * as React from 'react';
-import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Text, View, Button } from 'react-native';
+// import { createDrawerNavigator } from '@react-navigation/drawer';
 import TableauBord from './src/component/pages/TableauBord.js';
 import Lesson from './src/component/pages/Lesson.js';
 import Exercice from './src/component/pages/Exercice.js';
 
 const App = () => {
 
+  // const Menu = createDrawerNavigator();
   const Stack = createNativeStackNavigator();
   
   return (
       <NavigationContainer>
+
+        {/* <Menu.Navigator>
+          <Menu.Screen name="Lessons" component={Lesson} />
+        </Menu.Navigator> */}
+
         <Stack.Navigator>
           <Stack.Screen name="TableauBord" component={TableauBord} />
           <Stack.Screen name="Lesson" component={Lesson} />
           <Stack.Screen name="Exercice" component={Exercice} />
         </Stack.Navigator>
+
       </NavigationContainer>
   );
 }

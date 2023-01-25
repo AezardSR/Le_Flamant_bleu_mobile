430740f7f706d76e5c7c0a79827d94f87d567cea
Manon POTEL <manonpotel@hotmail.fr>
2023-01-25 11:33:41 +0100

    Fin mise en place du tableau de bord + Menu bottom

--- App.js
+++ App.js
@@ -3,10 +3,14 @@ import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faChalkboard, faGraduationCap, faHouse } from '@fortawesome/free-solid-svg-icons';
+import {  } from '@fortawesome/free-solid-svg-icons';
 // import { createDrawerNavigator } from '@react-navigation/drawer';
 import TableauBord from './src/component/pages/TableauBord.js';
 import Lesson from './src/component/pages/Lesson.js';
 import Exercice from './src/component/pages/Exercice.js';
+
 
 const App = () => {
 
@@ -28,9 +32,30 @@ const App = () => {
         </Stack.Navigator> */}
 
         <Tab.Navigator>
-          <Tab.Screen name="TableauBord" component={TableauBord} />
-          <Tab.Screen name="Lesson" component={Lesson} />
-          <Tab.Screen name="Exercice" component={Exercice} />
+
+          <Tab.Screen name="Lesson" component={Lesson} options={{
+            title: 'Leçons',
+            tabBarLabel: 'Leçons',
+            tabBarIcon: () => (
+              <FontAwesomeIcon icon={faGraduationCap} />
+              ),
+            }} />
+
+            <Tab.Screen name="TableauBord" component={TableauBord} options={{
+              title: 'Bienvenu "Prénom"',
+              tabBarLabel: 'Tableau de bord',
+              tabBarIcon: () => (
+                <FontAwesomeIcon icon={faHouse} />
+              ),
+            }} />
+            
+          <Tab.Screen name="Exercice" component={Exercice} options={{
+            title: 'Exercices',
+            tabBarLabel: 'Exercices',
+            tabBarIcon: () => (
+              <FontAwesomeIcon icon={faChalkboard} />
+            ),
+          }} />
         </Tab.Navigator>
 
       </NavigationContainer>

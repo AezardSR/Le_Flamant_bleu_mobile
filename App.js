7549f7be59975706fb7a0b98ee748160af488f47
Manon POTEL <manonpotel@hotmail.fr>
2023-01-25 09:33:09 +0100

    Ajout du menu bottom

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // import { createDrawerNavigator } from '@react-navigation/drawer';
 import TableauBord from './src/component/pages/TableauBord.js';
@@ -11,6 +12,7 @@ const App = () => {
 
   // const Menu = createDrawerNavigator();
   const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
   
   return (
       <NavigationContainer>
@@ -19,11 +21,17 @@ const App = () => {
           <Menu.Screen name="Lessons" component={Lesson} />
         </Menu.Navigator> */}
 
-        <Stack.Navigator>
+        {/* <Stack.Navigator>
           <Stack.Screen name="TableauBord" component={TableauBord} />
           <Stack.Screen name="Lesson" component={Lesson} />
           <Stack.Screen name="Exercice" component={Exercice} />
-        </Stack.Navigator>
+        </Stack.Navigator> */}
+
+        <Tab.Navigator>
+          <Tab.Screen name="TableauBord" component={TableauBord} />
+          <Tab.Screen name="Lesson" component={Lesson} />
+          <Tab.Screen name="Exercice" component={Exercice} />
+        </Tab.Navigator>
 
       </NavigationContainer>
   );

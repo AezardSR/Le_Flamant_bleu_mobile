9a6bc65adffd173199c621a9dff80d4f7ac8f264
AezardSR <69091066+AezardSR@users.noreply.github.com>
2023-01-26 18:58:31 +0100

    avancer sur la connexion

--- src/component/pages/TableauBord.js
+++ src/component/pages/TableauBord.js
@@ -1,15 +1,17 @@
-import * as React from 'react';
+import  React, {useContext} from 'react';
 import {NavigationContainer, useNavigation} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Text, View, Button, Image } from 'react-native';
+import { ApiContext } from '../features/loginAPi/ApiContext';
 // import PlanningIMG from '../../css/planningbanniere.png';
 
-class TableauBord extends React.Component {
-    render() {
-    // const navigation = useNavigation();
+export default function TableauBord() {
+    const {user} = useContext(ApiContext);
+    console.log(user, "user");
 
     return (
       <View>
+          <Text>{user.firstname}</Text>
         <View style={{position: 'relative', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
           <Image source={{uri: '../../css/planningbanniere.png'}} style={{width: 200, height: 200}} />
           <Text style={{position: 'absolute'}}>Planning</Text>
@@ -30,6 +32,3 @@ class TableauBord extends React.Component {
       </View>
     );
   }
-}
-
-export default TableauBord;

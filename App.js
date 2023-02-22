65f1d50181b6f74b91c95ed3dfa11309d64db3f9
AezardSR <theplaygamer@gmail.com>
2023-02-22 14:01:05 +0100

    Revert "merge tb in login"
    This reverts commit 02da75df9d8da4aaf42c71516b8683350d1fe827.

--- App.js
+++ App.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, {Component, useContext } from 'react';
 import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faChalkboard, faGraduationCap, faHouse } from '@fortawesome/free-solid-svg-icons';
+import {  } from '@fortawesome/free-solid-svg-icons';
+// import { createDrawerNavigator } from '@react-navigation/drawer';
+import TableauBord from './src/component/pages/TableauBord.js';
+import Lesson from './src/component/pages/Lesson.js';
+import Exercice from './src/component/pages/Exercice.js';
+
 import { ApiContext, ApiProvider } from './src/component/features/loginAPi/ApiContext.js';
 import Index from './src/Index.js';
 

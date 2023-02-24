1dca4ec712a67db9a4f0d170dcc43f5095399767
Manon POTEL <manonpotel@hotmail.fr>
2023-02-24 09:48:16 +0100

    Planning

--- src/component/pages/Planning.js
+++ src/component/pages/Planning.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Calendar, Agenda, LocaleConfig} from 'react-native-calendars';
 import { Text, View, Button, SafeAreaView, ScrollView, StyleSheet, TouchableOpacity, } from 'react-native';
 import moment from "moment";
@@ -29,24 +29,44 @@ LocaleConfig.defaultLocale = 'fr';
 const today = moment().format('YYYY-MM-DD');
 
 const Plannig = (props) => {
+
+    const [appointments, setAppointments] = useState([]);
+
+    useEffect(() => {
+      fetch('http://192.168.1.39:8000/api/appointments/')
+      .then(response => response.json())
+      .then(data => setAppointments(data))
+    }, [])
+
     // const navigation = useNavigation();
     const navigation = useNavigation();
     return (
       <SafeAreaView style={styles.container}>
           <Agenda
               items={{
-                '2023-01-26': [{name: "J'en peux"}, {name: "plus de"}, {name: "ce module"}],
+                '2023-02-24': [{name: "J'en peux", description: "plus de"}],
                 '2023-01-25': [{name: 'Mission'}]
               }}
 
               renderItem={(item, isFirst) => (
                 <TouchableOpacity style={styles.item}>
                   <Text style={styles.itemText}>{item.name}</Text>
+                  <Text style={styles.itemText}>{item.description}</Text>
                 </TouchableOpacity>
               )}
           />
 
           <Button title="Ajout" onPress={() => navigation.navigate('AddAppointments')}/>
+
+          {appointments.map((appointment) => {
+            return (
+              <View key={appointment.id}>
+                <Text>{appointment.titleDetails}</Text>
+                <Text>{appointment.descriptionDetails}</Text>
+              </View>
+            )
+          })}
+
       </SafeAreaView>
     );
 

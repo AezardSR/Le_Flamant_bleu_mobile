14a8eafa123c95c105e9c3e5848bd643fa4ffe8f
AezardSR <theplaygamer@gmail.com>
2023-03-21 15:40:43 +0100

    url remplacer par env variable

--- src/features/AddAppointments.js
+++ src/features/AddAppointments.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import {NavigationContainer, useNavigation} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Text, View, Button, ScrollView, TextInput, StyleSheet } from 'react-native';
+import {API_PATH} from '@env';
 
 const AddAppointments = () => {
   const [title, setTitle] = useState([]);
@@ -18,19 +19,19 @@ const AddAppointments = () => {
   const [typeAppoitmentsID, setTypeAppoitmentsID] = useState([]);
 
   useEffect(() => {
-      fetch('http://localhost:8000/api/user')
+      fetch(`${API_PATH}/use`)
       .then(response => response.json())
       .then(data => setReceiver(data))
   }, [])
 
   useEffect(() => {
-      fetch('http://localhost:8000/api/user')
+      fetch(`${API_PATH}/user`)
       .then(response => response.json())
       .then(data => setCreated(data))
   }, [])
 
   useEffect(() => {
-      fetch('http://localhost:8000/api/appointmentstypes')
+      fetch(`${API_PATH}/appointmentstypes`)
       .then(response => response.json())
       .then(data => setTypeAppoitments(data))
   }, [])
@@ -42,7 +43,7 @@ const AddAppointments = () => {
           body: JSON.stringify({titleDetails: title, descriptionDetails: description, dateDetails: date, receiver_id: receiverID, create_id: createdID, appointments_types_id: typeAppoitmentsID})
       };
 
-      fetch('http://localhost:8000/api/appointments', requestOptions)
+      fetch(`${API_PATH}/appointments`, requestOptions)
           .then(response => response.json())
           .then(data => console.log(data))
           event.preventDefault();
